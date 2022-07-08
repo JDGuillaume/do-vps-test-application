052efb0dcef73100650995e782d2186cdab6347d
Joey <josephdguillaume@gmail.com>
2022-07-08 17:14:01 -0400

    Initial app constructed.

---
+++ src/models/index.js
@@ -0,0 +1,28 @@
+const users = {
+  1: {
+    id: '1',
+    username: 'Mister Rogers',
+  },
+  2: {
+    id: '2',
+    username: 'Daniel Tiger',
+  },
+};
+
+const messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'Bye World',
+    userId: '2',
+  },
+};
+
+export default {
+  users,
+  messages,
+};
